4af6de69d0f412d273d2a55fa32b6dc18edb5bae
botmike <26753872+botmike@users.noreply.github.com>
2026-09-23 09:03:36 +0100

    moving things around

--- keyword_driven/generic/support/vars.ts
+++ keyword_driven/generic/support/vars.ts
@@ -2,6 +2,7 @@
 
 import { test as base } from 'playwright-bdd';
 import { expect } from '@playwright/test';
+import path from 'path';
 
 type Fixtures = { vars: Map<string, string> };
 
@@ -9,6 +10,9 @@ export const test = base.extend<Fixtures>({
   vars: async ({}, use, testInfo) => {
     const vars = new Map<string, string>();
     vars.set('browserName', testInfo.project.name);
+    vars.set('fixturePage', `file://${path.resolve(__dirname, '../fixtures/test-page.html')}`);
+    vars.set('fixturePage2', `file://${path.resolve(__dirname, '../fixtures/test-page-2.html')}`);
+
     await use(vars);
   },
 });
